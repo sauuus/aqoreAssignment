39d47f8094736d2b772779b093f6f32935af3d45
sauuus <sauuus>
2024-01-23 22:48:26 +0545

    Api: The Invoive Api

---
+++ BookingSystemAPI/WebAPI/WebAPI/Models/Invoice.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebAPI.Models
+{
+    public class Invoice
+    {
+        [Key]
+        public int InvoiceId { get; set; }
+        public int c_id { get; set; }
+        public DateTime InvoiceDate { get; set; }
+        public decimal TotalAmount { get; set; }
+        public decimal Discount { get; set; }
+        public decimal DiscountedAmount { get; set; }
+    }
+}
